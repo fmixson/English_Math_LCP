94be2140a6dc80931cb475f958d9c311c4693720
fmixson <flmixson@cerritos.edu>
2023-05-13 09:42:43 -0700

    ReportGenerator Commit

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import pandas as pd
 import openpyxl
 
+
 class MathEnglish:
-    def __init__(self, id, df):
+    def __init__(self, id, student_df):
         self.id = id
-        self.df = df
+        self.student_df = student_df
 
     def English_enrolled(self):
         english_course = None
         math_course = None
-        math_courses = ['MATH-104', 'MATH-110A','MATH-112','MATH-112S', 'MATH-114', 'MATH-116', 'MATH-140']
-        student_df = self.df[df['Student ID'] == id]
-        student_df = student_df.reset_index()
-        for i in range(len(student_df)):
-            if student_df.loc[i, 'Course Number'] == 'ENGL-100':
-                english_course = df.loc[i, 'Course Number']
+        math_courses = ['MATH-104', 'MATH-110A', 'MATH-112', 'MATH-112S', 'MATH-114', 'MATH-116', 'MATH-140']
+        english_courses = ['ENGL-100', 'ENGL-100S']
+        for i in range(len(self.student_df)):
+            if self.student_df.loc[i, 'Course Number'] in english_courses:
+                english_course = self.student_df.loc[i, 'Course Number']
 
-            if student_df.loc[i, 'Course Number'] == 'ENGL-100S':
-                english_course = df.loc[i, 'Course Number']
-
-            if student_df.loc[i, 'Course Number'] in math_courses:
-                math_course = student_df.loc[i, 'Course Number']
+            if self.student_df.loc[i, 'Course Number'] in math_courses:
+                math_course = self.student_df.loc[i, 'Course Number']
 
         if english_course == None:
             english_course = 'None'
@@ -28,18 +25,25 @@ class MathEnglish:
             math_course = 'None'
         return english_course, math_course, self.id
 
+
+class LCPsEdplans:
+
+    def __init__(self, student_df):
+        self.student_df = student_df
+
     def lcp(self):
-        head_count_df = self.df.drop_duplicates(subset='Student ID').reset_index
+        # head_count_df = self.df.drop_duplicates(subset='Student ID').reset_index
         ed_plans = ['ASEP: Abbreviated Educational Plan', 'CSEP: Comprehensive Educational Plan']
-        lcps = ['Arts, Humanities, & Communication', 'Applied Technology & Skilled Trades', 'Health Sciences & Wellness',
+        lcps = ['Arts, Humanities, & Communication', 'Applied Technology & Skilled Trades',
+                'Health Sciences & Wellness',
                 'Social & Behavioral Sciences', 'Science, Engineering, & Math', 'Business, Accounting, & Law',
                 'Education & Human Services', 'Exploration & Discovery']
         categorie_list = []
-        for i in range(len(self.df)):
+        for i in range(len(self.student_df)):
             lcp = None
             ed_plan = None
-            categorie_list.append(self.df.loc[i, 'Categories'])
-            string='  '.join([str(item) for item in categorie_list])
+            categorie_list.append(self.student_df.loc[0, 'Categories'])
+            string = '  '.join([str(item) for item in categorie_list])
             string = string.split(',')
             for item in string:
                 if item in ed_plans:
@@ -53,6 +57,7 @@ class MathEnglish:
                 lcp = 'None'
             return ed_plan, lcp
 
+
 class ReportGenerator:
 
     def __init__(self, id, lcp, ed_plan, english_course, math_course, dataframe):
@@ -64,7 +69,6 @@ class ReportGenerator:
         self.lcp_df = dataframe
 
     def english_math_dataframe(self):
-
         length = len(lcp_df)
         self.lcp_df.loc[length, 'Student ID'] = self.id
         self.lcp_df.loc[length, 'LCP'] = self.lcp
@@ -74,9 +78,10 @@ class ReportGenerator:
         return lcp_df
 
 
-columns = ['Student ID', 'LCP', 'Ed Plan','English', 'Math']
+columns = ['Student ID', 'LCP', 'Ed Plan', 'English', 'Math']
 lcp_df = pd.DataFrame(columns=columns)
-df = pd.read_csv('C:/Users/fmixson/Desktop/Dashboard_files/LCP_English_Math/campus-v2report-enrollment-2023-05-11 (1).csv')
+df = pd.read_csv(
+    'C:/Users/fmixson/Desktop/Dashboard_files/LCP_English_Math/campus-v2report-enrollment-2023-05-11 (1).csv')
 pd.set_option('display.max_columns', None)
 df = df[df['Dropped'] == 'No'].reset_index()
 id_list = []
@@ -85,14 +90,18 @@ for i in range(len(df)):
         id_list.append(df.loc[i, 'Student ID'])
 
 for id in id_list:
-    student = MathEnglish(id=id, df=df)
-    ed_plan, lcp = student.lcp()
+    student_df = df[df['Student ID'] == id]
+    student_df = student_df.reset_index()
+    student = MathEnglish(id=id, student_df=student_df)
     engl_course, math_course, id = student.English_enrolled()
+    lcpedplans = LCPsEdplans(student_df=student_df)
+    ed_plan, lcp = lcpedplans.lcp()
+
     print('English:', engl_course, 'Math:', math_course, id)
-    report = ReportGenerator(id=id, lcp=lcp, ed_plan=ed_plan,english_course=engl_course, math_course=math_course, dataframe = lcp_df)
+    report = ReportGenerator(id=id, lcp=lcp, ed_plan=ed_plan, english_course=engl_course, math_course=math_course,
+                             dataframe=lcp_df)
     lcp_df = report.english_math_dataframe()
     print(lcp_df)
 lcp_df.to_excel('EM_df.xlsx')
 # df[['First', 'Second', 'Third']] = df.Dropped.str.split(',', expand=True)
 # print(df)
-
