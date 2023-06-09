895b8d715bb841219c0225519430d7cc3342a3e2
fmixson <flmixson@cerritos.edu>
2023-06-09 16:15:16 -0700

    6-9-23 Commit

--- main.py
+++ main.py
@@ -1,108 +1,155 @@
 import pandas as pd
 import openpyxl
-
+import re
 
 class MathEnglish:
+    columns = ['Student ID', 'Student Name','Student Email', 'LCP', 'Major','Ed Plan', 'English', 'Math', 'Total Units', 'Course List', 'M/E']
+    lcp_df = pd.DataFrame(columns=columns)
+    ed_plans = ['ASEP: Abbreviated Educational Plan', 'CSEP: Comprehensive Educational Plan']
+    math_courses = ['MATH-104', 'MATH-110A', 'MATH-112', 'MATH-112S', 'MATH-114', 'MATH-116', 'MATH-140']
+    english_courses = ['ENGL-100', 'ENGL-100S']
+
     def __init__(self, id, student_df):
         self.id = id
+        self.df = df
         self.student_df = student_df
 
-    def English_enrolled(self):
-        english_course = None
-        math_course = None
-        math_courses = ['MATH-104', 'MATH-110A', 'MATH-112', 'MATH-112S', 'MATH-114', 'MATH-116', 'MATH-140']
-        english_courses = ['ENGL-100', 'ENGL-100S']
+    def enrollments(self):
+        english_course = 'None'
+        math_course = 'None'
+        total_units = 0
+        course_list = []
+
         for i in range(len(self.student_df)):
-            if self.student_df.loc[i, 'Course Number'] in english_courses:
+            if self.student_df.loc[i, 'Course Number'] in MathEnglish.english_courses:
                 english_course = self.student_df.loc[i, 'Course Number']
 
-            if self.student_df.loc[i, 'Course Number'] in math_courses:
+            if self.student_df.loc[i, 'Course Number'] in MathEnglish.math_courses:
                 math_course = self.student_df.loc[i, 'Course Number']
 
-        if english_course == None:
-            english_course = 'None'
-        if math_course == None:
-            math_course = 'None'
-        return english_course, math_course, self.id
 
+            if self.student_df.loc[i, 'Course Number'] not in course_list:
+                course_list.append(self.student_df.loc[i, 'Course Number'])
+                print(course_list)
+                total_units = total_units + self.student_df.loc[i, 'Credit Hours']
+                print(total_units)
 
-class LCPsEdplans:
+        # if english_course == None:
+        #     english_course = 'None'
+        # if math_course == None:
+        #     math_course = 'None'
+        return english_course, math_course, self.id, total_units, course_list
 
-    def __init__(self, student_df):
-        self.student_df = student_df
 
     def lcp(self):
-        # head_count_df = self.df.drop_duplicates(subset='Student ID').reset_index
-        ed_plans = ['ASEP: Abbreviated Educational Plan', 'CSEP: Comprehensive Educational Plan']
+
         lcps = ['Arts, Humanities, & Communication', 'Applied Technology & Skilled Trades',
                 'Health Sciences & Wellness',
                 'Social & Behavioral Sciences', 'Science, Engineering, & Math', 'Business, Accounting, & Law',
-                'Education & Human Services', 'Exploration & Discovery']
-        categorie_list = []
+                'Education & Human Services', 'Exploration & Discovery', 'VETS', 'Umoja', 'Transfer Academy']
+
         for i in range(len(self.student_df)):
             lcp = None
-            ed_plan = None
-            categorie_list.append(self.student_df.loc[0, 'Categories'])
-            string = '  '.join([str(item) for item in categorie_list])
-            string = string.split(',')
-            for item in string:
-                if item in ed_plans:
-                    ed_plan = item
-                if item in lcps:
-                    lcp = item
+            categorie = self.student_df.loc[0, 'Categories']
+            for lcp_item in lcps:
+                if re.search(pattern=lcp_item, string=categorie):
+                    lcp = lcp_item
+                    break
+                if lcp == None:
+                    lcp = 'None'
 
-            if ed_plan == None:
-                ed_plan = 'None'
-            if lcp == None:
-                lcp = 'None'
-            return ed_plan, lcp
+        return lcp
+
+    def ed_plan(self):
+
+        categorie_list = []
+        ed_plan = None
+        categorie_list.append(self.student_df.loc[0, 'Categories'])
+        string='  '.join([str(item) for item in categorie_list])
+        categorie_list.append(self.student_df.loc[0, 'Categories'])
+        string = '  '.join([str(item) for item in categorie_list])
+        string = string.split(',')
+        for item in string:
+            if item in MathEnglish.ed_plans:
+                ed_plan = item
+
+        return ed_plan
+
+    def report_generator(self, english, math, lcp, ed_plan, total_units, course_list):
+
+        length = len(MathEnglish.lcp_df)
+        MathEnglish.lcp_df.loc[length, 'Student ID'] = self.id
+        MathEnglish.lcp_df.loc[length, 'Student Name'] = self.student_df.loc[0, 'Student Name']
+        MathEnglish.lcp_df.loc[length, 'Student Email'] = self.student_df.loc[0, 'Student E-mail']
+        MathEnglish.lcp_df.loc[length, 'LCP'] = lcp
+        MathEnglish.lcp_df.loc[length, 'Major'] = self.student_df.loc[0, 'Major']
+        MathEnglish.lcp_df.loc[length, 'Ed Plan'] = ed_plan
+        MathEnglish.lcp_df.loc[length, 'English'] = english
+        MathEnglish.lcp_df.loc[length, 'Math'] = math
+        MathEnglish.lcp_df.loc[length, 'Total Units'] = total_units
+        MathEnglish.lcp_df.loc[length, 'Course List'] = course_list
 
 
-class ReportGenerator:
+        return MathEnglish.lcp_df
 
-    def __init__(self, id, lcp, ed_plan, english_course, math_course, dataframe):
-        self.id = id
-        self.lcp = lcp
-        self.ed_plan = ed_plan
-        self.english_course = english_course
-        self.math_course = math_course
-        self.lcp_df = dataframe
+def calculations(lcp_df):
 
-    def english_math_dataframe(self):
-        length = len(lcp_df)
-        self.lcp_df.loc[length, 'Student ID'] = self.id
-        self.lcp_df.loc[length, 'LCP'] = self.lcp
-        self.lcp_df.loc[length, 'Ed Plan'] = self.ed_plan
-        self.lcp_df.loc[length, 'English'] = self.english_course
-        self.lcp_df.loc[length, 'Math'] = self.math_course
-        return lcp_df
+    english_total_df = MathEnglish.lcp_df[MathEnglish.lcp_df['English'] != 'None']
+    english_total = len(english_total_df)
+    math_total_df = MathEnglish.lcp_df[MathEnglish.lcp_df['Math'] != 'None']
+    math_total = len(math_total_df)
+    for i in range(len(MathEnglish.lcp_df)):
+        eng = 'no'
+        math = 'no'
+        if MathEnglish.lcp_df.loc[i, 'English'] != 'None':
+            eng = 'yes'
+        if MathEnglish.lcp_df.loc[i, 'Math'] != 'None':
+            math = 'yes'
+
+        if eng == 'yes' and math == 'yes':
+            MathEnglish.lcp_df.loc[i, 'M/E'] = 'yes'
+    both_total_df = MathEnglish.lcp_df[MathEnglish.lcp_df['M/E'] == 'yes']
+    both_total = len(both_total_df)
+
+    print(english_total,math_total, both_total)
+
+def lcp_sheets(lcp_df):
+    lcps = ['Arts, Humanities, & Communication', 'Applied Technology & Skilled Trades',
+            'Health Sciences & Wellness','Social & Behavioral Sciences', 'Science, Engineering, & Math',
+            'Business, Accounting, & Law', 'Education & Human Services', 'Exploration & Discovery', 'VETS', 'Umoja',
+            'Transfer Academy']
+
+    for lcp_item in lcps:
+        lcp_df2 = lcp_df[lcp_df['LCP'] == lcp_item]
+        lcp_df2.to_excel(lcp_item + '.xlsx')
 
 
-columns = ['Student ID', 'LCP', 'Ed Plan', 'English', 'Math']
-lcp_df = pd.DataFrame(columns=columns)
-df = pd.read_csv(
-    'C:/Users/fmixson/Desktop/Dashboard_files/LCP_English_Math/campus-v2report-enrollment-2023-05-23(3).csv')
-
+df = pd.read_csv('C:/Users/fmixson/Desktop/Dashboard_files/LCP_English_Math/campus-v2report-enrollment-2023-05-23(3).csv')
+# df = pd.read_csv(
+#     'C:/Users/fmixson/Desktop/Dashboard_files/LCP_English_Math/campus-v2report-enrollment-2023-05-11 (1).csv')
+df.to_excel('test1.xlsx')
 pd.set_option('display.max_columns', None)
-df = df[df['Dropped?'] == 'No'].reset_index()
+df = df[df['Dropped?'] == 'No']
+sorted_df = df.sort_values(by='Student ID').reset_index()
+sorted_df.to_excel('test.xlsx')
 id_list = []
+print(df)
 for i in range(len(df)):
-    if df.loc[i, 'Student ID'] not in id_list:
-        id_list.append(df.loc[i, 'Student ID'])
+    if sorted_df.loc[i, 'Student ID'] not in id_list:
+        id_list.append(sorted_df.loc[i, 'Student ID'])
 
 for id in id_list:
-    student_df = df[df['Student ID'] == id]
+    student_df = sorted_df[sorted_df['Student ID'] == id]
     student_df = student_df.reset_index()
     student = MathEnglish(id=id, student_df=student_df)
-    engl_course, math_course, id = student.English_enrolled()
-    lcpedplans = LCPsEdplans(student_df=student_df)
-    ed_plan, lcp = lcpedplans.lcp()
-
-    print('English:', engl_course, 'Math:', math_course, id)
-    report = ReportGenerator(id=id, lcp=lcp, ed_plan=ed_plan, english_course=engl_course, math_course=math_course,
-                             dataframe=lcp_df)
-    lcp_df = report.english_math_dataframe()
-    print(lcp_df)
+    engl_course, math_course, id, total_units, course_list = student.enrollments()
+    lcp = student.lcp()
+    ed_plan = student.ed_plan()
+    lcp_df = student.report_generator(english=engl_course, math=math_course, lcp=lcp, ed_plan=ed_plan, total_units=total_units,
+                                      course_list=course_list)
+calculations(lcp_df=lcp_df)
+lcp_sheets(lcp_df=lcp_df)
 lcp_df.to_excel('EM_df.xlsx')
 # df[['First', 'Second', 'Third']] = df.Dropped.str.split(',', expand=True)
+# print(df)d', 'Third']] = df.Dropped.str.split(',', expand=True)
 # print(df)
